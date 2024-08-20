f16164cd08f326b3ad04b43701afbb65a1d18129
dgmeyer3 <2don.meyer@gmail.com>
2024-08-20 19:47:45 -0400

    add data to db

--- request.py
+++ request.py
@@ -6,20 +6,25 @@ from sqlalchemy import text
 from datetime import datetime
 
 #db: testdatabase
+#change intervalhours to an appropriate number of my choosing
+#count number of items in list
+#create column in db for each list item
+#load list to db 
 
 def main():
 
     type1 = "wave"
-    cupsogue_params = "spotId=5842041f4e65fad6a77089e8&days=1&intervalHours=12"
+    cupsogue_params = "spotId=5842041f4e65fad6a77089e8&days=1&intervalHours=6"
     base_url = f"https://services.surfline.com/kbyg/spots/forecasts/"
     pg_username = "tester"
-    pg_password = "not real password"
+    pg_password = " "
     
     #{type1}?{cupsogue_params}
 
-    #cupsogue_data_dict = request(base_url, type1, cupsogue_params)
+    cupsogue_data_dict = request(base_url, type1, cupsogue_params)
+
     #display(cupsogue_data_dict)
-    connect_to_db(pg_username,pg_password)
+    connect_to_db(pg_username,pg_password,cupsogue_data_dict)
 
 def request(url,type_data,params):
 
@@ -51,15 +56,21 @@ def display(info_to_display):
     
     if str(dataType) == "<class 'list'>":
         print(json.dumps(info_to_display,indent=2))
+        print(len(info_to_display))
         #print(swell_data[0])
+
     else:
         print("the requested data was not a list, and"
                "could not be displayed with this function.")
         
-def connect_to_db(user, password):
+def connect_to_db(user,password,data_for_db):
     db = create_engine(f"postgresql+psycopg2://{user}:{password}@localhost:5432/testdatabase", echo = True)
     with db.connect() as conn:
-        result = conn.execute(text("select 'test'"))
+        for item in data_for_db:
+            conn.execute("INSERT into testdatabase(12AM,6AM,12PM,6PM) VALUES (%s, %s, %s, %s)", item)
+
+        conn.commit()
+        conn.close()
         
 if __name__ == "__main__":
     main()
