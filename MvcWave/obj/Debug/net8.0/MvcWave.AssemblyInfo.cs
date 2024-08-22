6d5fef69ffebe0edaf5b0ca72edba66f83b0902e
dgmeyer3 <2don.meyer@gmail.com>
2024-08-22 17:22:29 -0400

    safety commit

--- MvcWave/obj/Debug/net8.0/MvcWave.AssemblyInfo.cs
+++ MvcWave/obj/Debug/net8.0/MvcWave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcWave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a4c2bfded156029e42837613b51a92e1bd61b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d792b274a59075a1b10c9bc54b362c11969e381")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcWave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcWave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
